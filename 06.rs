3fea8ff18d719e20944f625260bcc5dce7fb3c0c
TheAlgorythm <mos-news@web.de>
2020-12-06 16:45:00 +0100

    06: simplification & performance improved by factor 6

--- 06.rs
+++ 06.rs
@@ -1,77 +1,24 @@
-use std::collections::BTreeSet;
-
-struct Unique<I>
-where
-    I: Iterator,
-{
-    seen: BTreeSet<I::Item>,
-    underlying: I,
-}
-
-impl<I> Iterator for Unique<I>
-where
-    I: Iterator,
-    I::Item: Ord + Clone,
-{
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while let Some(x) = self.underlying.next() {
-            if !self.seen.contains(&x) {
-                self.seen.insert(x.clone());
-                return Some(x);
-            }
-        }
-        None
-    }
-}
-
-trait UniqueExt: Iterator {
-    fn unique(self) -> Unique<Self>
-    where
-        Self::Item: Ord + Clone,
-        Self: Sized,
-    {
-        Unique {
-            seen: BTreeSet::new(),
-            underlying: self,
-        }
-    }
-}
-
-impl<I: Iterator> UniqueExt for I {}
-
 fn parse_answers(input: &str) -> Vec<&str> {
     input.split("\n\n").collect()
+}
+
+fn count_anyone_answer(group: &str) -> usize {
+    ('a'..='z')
+        .filter(|character: &char| group.contains(*character))
+        .count()
 }
 
 fn solve_part_one(answers: &Vec<&str>) {
     let answer_count_sum: usize = answers
         .iter()
-        .map(|group| {
-            group
-                .chars()
-                .filter(char::is_ascii_alphabetic)
-                .unique()
-                .count()
-        })
+        .map(|group| count_anyone_answer(&group))
         .sum();
     println!("The total answers count summed up is {}.", answer_count_sum);
 }
 
 fn count_everyone_answer(group: &str) -> usize {
-    let people = group.lines().count();
-    group
-        .chars()
-        .filter(char::is_ascii_alphabetic)
-        .unique()
-        .filter(|answer_type| {
-            group
-                .chars()
-                .filter(|answers| answers == answer_type)
-                .count()
-                == people
-        })
+    ('a'..='z')
+        .filter(|character: &char| group.lines().all(|form| form.contains(*character)))
         .count()
 }
 
