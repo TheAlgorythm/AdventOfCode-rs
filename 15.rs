83e54aeecf1f36a2432ab3686846aef8fe83efbe
ZSchoen <dev@zschoen.dev>
2021-03-03 12:43:51 +0100

    improved performance 15 / van Eck

--- 15.rs
+++ 15.rs
@@ -1,3 +1,5 @@
+#![feature(min_const_generics)]
+
 use std::collections::HashMap;
 
 fn parse_num_list(input: &str) -> Vec<u32> {
@@ -8,25 +10,69 @@ fn parse_num_list(input: &str) -> Vec<u32> {
         .expect("Couldn't parse input!")
 }
 
+struct SplittedMap<V, const STATIC_SIZE: usize> {
+    hot_table: [Option<V>; STATIC_SIZE],
+    pub cold_map: HashMap<usize, V>,
+}
+
+impl<V, const STATIC_SIZE: usize> SplittedMap<V, STATIC_SIZE>
+where
+    V: Copy,
+    Option<V>: Copy,
+{
+    fn new() -> Self {
+        Self {
+            hot_table: [None; STATIC_SIZE],
+            cold_map: HashMap::new(),
+        }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            hot_table: [None; STATIC_SIZE],
+            cold_map: HashMap::with_capacity(capacity),
+        }
+    }
+
+    fn get(&self, key: &usize) -> Option<V> {
+        if *key < STATIC_SIZE {
+            return self.hot_table[*key];
+        } else {
+            return self.cold_map.get(key).map(|value| *value);
+        }
+    }
+
+    fn set(&mut self, key: usize, value: V) {
+        if key < STATIC_SIZE {
+            self.hot_table[key] = Some(value);
+        } else {
+            self.cold_map.insert(key, value);
+        }
+    }
+}
+
 fn get_nth_number(starting_values: &Vec<u32>, nth: usize) -> u32 {
-    let mut last_values: HashMap<u32, u32> = starting_values
+    let mut last_values: SplittedMap<u32, { 1024 * 2048 }> =
+        SplittedMap::with_capacity(2039983 * (nth > { 1024 * 2048 }) as usize);
+    starting_values
         .iter()
         .take(starting_values.len() - 1)
         .enumerate()
-        .map(|(index, value)| (*value, index as u32 + 1))
-        .collect();
+        .for_each(|(index, value)| last_values.set(*value as usize, index as u32 + 1));
 
-    let mut last_value = *starting_values.last().expect("No starting value!");
+    let mut last_value = *starting_values.last().expect("No starting value!") as usize;
 
     (starting_values.len()..nth).for_each(|index| {
-        let current_value = match last_values.get(&last_value) {
-            Some(last_occuring_index) => index as u32 - last_occuring_index,
-            None => 0,
-        };
-        last_values.insert(last_value, index as u32);
-        last_value = current_value;
+        let current_value = last_values
+            .get(&last_value)
+            .map(|last_occuring_index| index as u32 - last_occuring_index)
+            .unwrap_or(0);
+
+        last_values.set(last_value, index as u32);
+        last_value = current_value as usize;
     });
-    last_value
+    println!("{} hash-entrys", last_values.cold_map.len());
+    last_value as u32
 }
 
 fn solve_part_one(starting_values: &Vec<u32>) {
